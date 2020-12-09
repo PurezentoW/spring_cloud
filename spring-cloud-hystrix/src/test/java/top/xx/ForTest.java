cc9b91eca2c530363d21539463891ee55431406c
silence-tea <silenceteaaaaa@gmail.com>
2020-12-09 14:39:26 +0800

    20201209

---
+++ spring-cloud-hystrix/src/test/java/top/xx/ForTest.java
@@ -0,0 +1,21 @@
+package top.xx;
+
+import org.junit.Test;
+
+public class ForTest {
+    @Test
+    public void mmm() {
+        System.out.println(this.returnTest());
+    }
+
+    private String returnTest(){
+        String[] str = {"1","2","3","4","6","7"};
+        boolean mark = false;
+        for(String s:str){
+            if(s.equals("1")){
+                return null;
+            }
+        }
+        return String.valueOf(mark);
+    }
+}
